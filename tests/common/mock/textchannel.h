a7b1ee3629b40e2622a4d01a214b30daec0975ea
Adam Boardman <adamboardman@gmail.com>
2019-01-11 21:50:35 +0000

    Fixes for update to telpathy-qt 9.7

--- tests/common/mock/textchannel.h
+++ tests/common/mock/textchannel.h
@@ -45,7 +45,11 @@ public:
     void addMembers(QStringList recipients);
     QStringList recipients() const;
     Tp::UIntList members();
-    void onRemoveMembers(const Tp::UIntList& handles, const QString& message, Tp::DBusError* error);
+#if TP_QT_VERSION >= TP_QT_VERSION_CHECK(0, 9, 7)
+    void onRemoveMembers(const Tp::UIntList &handles, const QString &message, uint reason, Tp::DBusError *error);
+#else
+    void onRemoveMembers(const Tp::UIntList &handles, const QString &message, Tp::DBusError *error);
+#endif
     void onAddMembers(const Tp::UIntList& handles, const QString& message, Tp::DBusError* error);
 
 public Q_SLOTS:
